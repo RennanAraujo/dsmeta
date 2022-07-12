fb25a4442bba9e948507e86d2b2278a1dfe72b57
Rennan Araujo <rennansoarescosta@gmail.com>
2022-07-11 21:10:42 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
-import NotificationButton from './components/NotificationButton';
+import Header from './components/Header';
+import SalesCard from './components/SalesCard';
 
 function App() {
   return (
     <>
-      <h1>Hey!</h1>
-      <NotificationButton />
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="ds-meta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   );
 }
