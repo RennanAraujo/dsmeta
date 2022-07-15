a83fb3518d82fdd6ac705a8174267e51b66b5c50
Rennan Araujo <rennansoarescosta@gmail.com>
2022-07-15 19:56:53 -0300

    Sale listing

---
+++ frontend/src/utils/request.ts
@@ -0,0 +1,4 @@
+export const BASE_URL =
+  import.meta.env.VITE_BACKEND_URL ?? 'http://localhost:8080';
+//operador de coalenscência nula
+//pega  o valor da variavel de ambiente, se não tiver, use localhost:8080
